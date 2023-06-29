7e293d63a2bcbc356f4d88d34c07404eac262396
Renan Camacho <86194197+rcamacho89@users.noreply.github.com>
2023-06-29 14:18:31 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
-#provider "oci" {
+provider "oci" {
 #  region               = var.region
 #  tenancy_ocid         = var.tenancy_ocid
-#  user_ocid            = var.user_ocid
+  user_ocid            = var.user_ocid
 #  fingerprint          = var.fingerprint
 #  private_key_path     = var.private_key_path
-#}
+}
 
  #terraform {
  # required_version = ">= 1.1.0"
